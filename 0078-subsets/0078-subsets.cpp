e257bebb74be305ab82120fc2d97dcaa96b18ecf
Abhinav Ghosh <118923507+abhinav8311@users.noreply.github.com>
2025-02-06 18:37:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.36%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int>subset;
+        vector<vector<int>> result;//stores entire result
+        vector<int>subset;//stores current subset
 
         dfs(nums,0,subset,result);
         return result;
@@ -10,14 +10,14 @@ public:
 
     void dfs(vector<int> &nums, int i, vector<int> &subset, vector<vector<int>> &result){
         if(i>=nums.size()){
-            result.push_back(subset);
+            result.push_back(subset);//if i reaches final subset, store it and return
             return;
         }
 
-        subset.push_back(nums[i]);
+        subset.push_back(nums[i]);//1stly -> include the element and then recure
         dfs(nums,i+1,subset,result);
 
-        subset.pop_back();
+        subset.pop_back();//then -> exlude the element and then recure
         dfs(nums,i+1,subset,result);
     }
 };
