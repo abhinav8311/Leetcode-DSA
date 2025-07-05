80ee1de09d88fdef07d3f86e10e3040199a6816a
Abhinav Ghosh <118923507+abhinav8311@users.noreply.github.com>
2025-07-06 00:51:54 +0530

    Time: 0 ms (100%), Space: 11.9 MB (79.51%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -22,23 +22,24 @@ public:
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*, Node*> oldToNew;
-        queue<Node*> q;
+        if(!node) return NULL;
 
-        if(!node) return nullptr;
+        unordered_map<Node* , Node*> oldToNew;
+        queue<Node*>q;
 
-        oldToNew[node]=new Node(node->val);
+        oldToNew[node] = new Node(node->val);
         q.push(node);
 
         while(!q.empty()){
             Node* cur=q.front();
             q.pop();
 
-            for(Node* nei : cur->neighbors){
+            for(Node* nei: cur->neighbors){
                 if(oldToNew.find(nei)==oldToNew.end()){
-                    oldToNew[nei]=new Node(nei->val);
+                    oldToNew[nei] = new Node(nei->val);
                     q.push(nei);
                 }
+
                 oldToNew[cur]->neighbors.push_back(oldToNew[nei]);
             }
         }
