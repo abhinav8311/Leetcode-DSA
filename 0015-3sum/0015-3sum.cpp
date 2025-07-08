684f376ba3cbc2362517fdc1b189d76dbc0de1ee
Abhinav Ghosh <118923507+abhinav8311@users.noreply.github.com>
2025-07-08 18:52:37 +0530

    Time: 44 ms (80.01%), Space: 29.1 MB (51.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
         int n=nums.size();
+        vector<vector<int>>result;
 
-        if(n<3) return result;//if less than three elements , no triplets
+        sort(nums.begin(), nums.end());
 
-        sort(nums.begin(), nums.end());//sort it to find duplicates 
+        if(n<3 || nums[0]>0) return result;
 
         for(int i=0;i<n;i++){
-            if(nums[i]>0) //if the 1st element is greater than zero, just break coz array is sorted
-                break;
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
 
-            if(i>0 && nums[i]==nums[i-1]) //from the 2nd element , if the value is same as the previous one
-                continue;//skip the iteration
+            int target = -nums[i];
 
-            int l=i+1;//take two pointers
-            int r=n-1;
+            int L=i+1; int R=n-1;
 
-            while(l<r)
-            {
-                int sum=nums[i]+nums[l]+nums[r];//follow two sum with sorted array
+            while(L<R){
+                int sum=nums[L]+nums[R];
 
-                if(sum>0){
-                    r--;
-                }
-                else if(sum<0){
-                    l++;
-                }
-                else{//if sum==0
-                    result.push_back({nums[i],nums[l],nums[r]});//push the values in result
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
+                if(sum==target){
+                    result.push_back({nums[i],nums[L],nums[R]});
+
+                    while(L<R && nums[L]==nums[L+1]){
+                        L++;
                     }
+                    L++;
+                    R--;
+                }
+                else if(sum<target){
+                    L++;
+                }
+                else{
+                    R--;
                 }
             }
         }
