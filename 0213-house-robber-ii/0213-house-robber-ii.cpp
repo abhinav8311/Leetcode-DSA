86342c008f62ec7df7dc3f7b52c522448162ab9d
Abhinav Ghosh <118923507+abhinav8311@users.noreply.github.com>
2025-07-28 11:29:03 +0530

    Time: 0 ms (100%), Space: 11 MB (8.44%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        if(nums.size()==1) return nums[0];
+        
+        vector<int> includeFirst(nums.begin() , nums.end()-1);
+        vector<int> includeLast(nums.begin()+1 , nums.end());
+
+        return max(helper(includeFirst) , helper(includeLast));
+    }
+
+private:
+    int helper(vector<int>& nums) {
+
+        if(nums.size()==1) return nums[0];
+
+        int rob1=nums[0];
+        int rob2=max(nums[0],nums[1]);
+
+        for(int i=2;i<nums.size();i++){
+            int temp = max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+
+        return rob2;
+    }
+};
