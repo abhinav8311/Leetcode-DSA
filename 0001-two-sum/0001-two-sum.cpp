5dc0755f15294dcb86523f2053e9b13c3b3ee9ab
Abhinav Ghosh <118923507+abhinav8311@users.noreply.github.com>
2025-09-12 18:52:23 +0530

    Time: 3 ms (70.2%), Space: 14.9 MB (20.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> map;//declare a map
-        for(int i=0;i<nums.size();i++)
-        {
-            int diff=target-nums[i];//for each element calculate its compliment value
-            if(map.find(diff)!=map.end()){//if that compliment is avalaible, return the complimnet and current index
-                return {map[diff],i};
-            }
+       unordered_map<int,int> m;
 
-            map.insert({nums[i],i});//else enter that value and position in the map
-        }
-        return {};
+       for(int i=0;i<nums.size();i++){
+        int comp = target - nums[i];
+            if(m.find(comp)!=m.end()){
+                return {m[comp] , i};
+            }
+            m.insert({nums[i],i});
+       }
+
+       return {};
     }
 };
