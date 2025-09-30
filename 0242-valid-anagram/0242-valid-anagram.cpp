00d162b2ef42744104e890ffeb18d239f74c5323
Abhinav Ghosh <118923507+abhinav8311@users.noreply.github.com>
2025-10-01 02:32:27 +0530

    Time: 0 ms (100%), Space: 10 MB (11.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> map;
-        if(s.size()!=t.size())//if number of char not equal , not anagram
-        return false;
-        for(int i=0;i<s.size();i++)//traverse through any size coz both must be same
-        {
-            map[s[i]]++;//increase count for each character of s
-            map[t[i]]--;//decrease count for each character of t
-        }//map key must be zero if both strings had same characters
-
-        for(auto it:map)//auto iterate through map
-        {
-            if(it.second)//if there is any value to key, not anagram
+        unordered_map<char,int>mp;
+        if(s.length()!=t.length()){
             return false;
         }
         
-        return true;//true if key is zero
+        int n = s.length();
+
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto it:mp){
+            if(it.second > 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
